6ddf7ba24108130981b12cf30c08f84dcb1aa57f
Tim Pepper <tpepper@vmware.com>
2018-03-27 13:54:30 -0700

    json instead of text?
    Seems there is some difference in the json, text and blob options.  The
    docs are soooo bad.
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -3,19 +3,12 @@ commit="8da5ea2a04664db616b115511403b71570341f74"
 url="https://github.com/"+repo+"/commit/"+commit+".patch"
 
 fetch(url, {
-	mode: 'no-cors'
+	mode: 'cors'
 })
 .then(function(response) {
-	return response.blob();
-})
-.then(function(kudoBlob) {
-	console.log("Blob size: "+kudoBlob.size);
-	console.log("Blob type: "+kudoBlob.type);
-	reader = new FileReader();
-	reader.onload = function(e) {
-		console.log("Blob content: "+e.target.result);
-	};
-	reader.readAsText(kudoBlob);
+	return response.json();
+}).then(function(json) {
+	console.log("json is: "+json);
 }).catch(function(error) {
 	console.log(error);
 	return "could not find kudo commit at "+url;
