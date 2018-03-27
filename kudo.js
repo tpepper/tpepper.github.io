98ff81ed62ff859c3cae10a1cca2e8adda044419
Tim Pepper <tpepper@vmware.com>
2018-03-27 11:17:31 -0700

    no CORS
    I'm so not a web aware developer.  But this kudos badge is indeed doing
    cross origin requests, so it needs to not break itself doing those.
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -1,7 +1,9 @@
 repo="tpepper/kudos-badge"
 commit="8da5ea2a04664db616b115511403b71570341f74"
 url="https://github.com/"+repo+"/commit/"+commit+".patch"
-fetch(url)
+fetch(url, {
+	mode: 'no-cors'
+})
 .then(function(response) {
 	return response.text();
 })
