1ae85646b89e65b6f1451e97c0b72a027ff16007
Tim Pepper <tpepper@vmware.com>
2018-03-27 11:10:24 -0700

    try to fetch the commit contents
    Fetch and display the commit contents or error message.
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -1,4 +1,10 @@
 repo="tpepper/kudos-badge"
 commit="8da5ea2a04664db616b115511403b71570341f74"
 url="https://github.com/"+repo+"/commit/"+commit+".patch"
-document.write(url)
+fetch(url)
+.then(function(response) {
+	return response.text();
+})
+.then(function(error) {
+	return "could not find kudo commit at "+url
+})
