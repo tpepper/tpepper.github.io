a5a29d6b853cc0da29244df143ef45efe743c14a
Tim Pepper <tpepper@vmware.com>
2018-03-27 14:00:44 -0700

    CORS is set on the server for the requested resource?
    This seems crazy.  If I wanted to serve malicious data, I'd set the server
    to allow it.  That doesn't sound like security.
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -3,7 +3,7 @@ commit="8da5ea2a04664db616b115511403b71570341f74"
 url="https://github.com/"+repo+"/commit/"+commit+".patch"
 
 fetch(url, {
-	mode: 'cors'
+	mode: 'no-cors'
 })
 .then(function(response) {
 	return response.json();
