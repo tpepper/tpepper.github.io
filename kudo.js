aa682a53f8b446d476f3b9868d56cbfeb99a0638
Tim Pepper <tpepper@vmware.com>
2018-03-27 14:13:32 -0700

    Use github API
    The GH API actually returns a "Access-Control-Allow-Origin: *" header and I
    get the expected json for the commit!!
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -1,14 +1,16 @@
 repo="tpepper/kudos-badge"
 commit="8da5ea2a04664db616b115511403b71570341f74"
-url="https://github.com/"+repo+"/commit/"+commit+".patch"
+url="https://api.github.com/repos/"+repo+"/commits/"+commit
 
 fetch(url, {
-	mode: 'no-cors'
+	mode: 'cors'
 })
 .then(function(response) {
 	return response.json();
 }).then(function(json) {
-	console.log("json is: "+json);
+	kudoJSON = JSON.stringify(json);
+	console.log("json is: "+kudoJSON);
+	return kudoJSON;
 }).catch(function(error) {
 	console.log(error);
 	return "could not find kudo commit at "+url;
