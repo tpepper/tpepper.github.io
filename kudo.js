52092a32f2325840425fed5923edc98e4a050b2c
Tim Pepper <tpepper@vmware.com>
2018-03-27 11:26:56 -0700

    try to handle errors
    Fetch is confusing.  Error handling in javascript is confusing.  Maybe
    this outputs something useful on failure?
    
    Signed-off-by: Tim Pepper <tpepper@vmware.com>

--- kudo.js
+++ kudo.js
@@ -1,12 +1,21 @@
 repo="tpepper/kudos-badge"
 commit="8da5ea2a04664db616b115511403b71570341f74"
 url="https://github.com/"+repo+"/commit/"+commit+".patch"
+
+function handleFetchErrors(response) {
+	if (!response.ok) {
+		throw Error(response.statusText);
+	}
+	return response;
+}
+
 fetch(url, {
 	mode: 'no-cors'
 })
+.then(handleFetchErrors)
 .then(function(response) {
 	return response.text();
-})
-.then(function(error) {
+}).catch(function(error) {
+	console.log(error);
 	return "could not find kudo commit at "+url
 })
